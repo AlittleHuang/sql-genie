bf154d998eebbacae09541ad68bf967b20ecd252
huangchengwei <huang.chengwei@outlook.com>
2024-03-19 15:36:33 +0800

    修复事务不生效的问题

--- spring-boot-starters/spring-boot-starter-sql-genie-common/src/main/java/io/github/genie/sql/data/access/TransactionalUpdate.java
+++ spring-boot-starters/spring-boot-starter-sql-genie-common/src/main/java/io/github/genie/sql/data/access/TransactionalUpdate.java
@@ -3,13 +3,19 @@ package io.github.genie.sql.data.access;
 import io.github.genie.sql.api.Update;
 import io.github.genie.sql.api.Updater;
 import io.github.genie.sql.builder.UpdaterImpl;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public class TransactionalUpdate implements Update {
+public class TransactionalUpdate implements Update, ApplicationContextAware {
 
     private final Update target;
+    private ApplicationContext applicationContext;
+    private Update update;
 
     public TransactionalUpdate(Update target) {
         this.target = target;
@@ -59,7 +65,19 @@ public class TransactionalUpdate implements Update {
 
     @Override
     public <T> Updater<T> getUpdater(Class<T> type) {
-        return new UpdaterImpl<>(this, type);
+        return new UpdaterImpl<>(getUpdate(), type);
     }
 
+    @NotNull
+    private Update getUpdate() {
+        if (update != null) {
+            return update;
+        }
+        return update = applicationContext.getBean(TransactionalUpdate.class);
+    }
+
+    @Override
+    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
